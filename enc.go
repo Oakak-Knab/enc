8cbfdd64ae5bb0a3e91213e12760bde1e9eda61b
pil9 <ljp3326@naver.com>
2025-08-12 03:11:51 +0900

    feat: This seemed like a brilliant idea at 3 AM.

---
+++ enc.go
@@ -0,0 +1,32 @@
+package enc
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func Encrypt(plainText []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	cypherText := gcm.Seal(nonce, nonce, plainText, nil)
+
+	return cypherText, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Oakak-Knab/enc
+
+go 1.24.1
